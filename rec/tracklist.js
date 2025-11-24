562be32274c03a1cd94704b4bf286affcfed7e54
Hayk <hayk@flodesk.com>
2025-11-24 22:29:26 +0100

    rec responsive

---
+++ rec/tracklist.js
@@ -0,0 +1,27 @@
+const tracklist = {
+  col1: [`01 Lullaby.mp3`, `07 Panic.mp3`],
+  col2: [`15 To the Operating Room_End Title.mp3`, `01.mp3`, `20 Track 20.mp3`],
+  col3: [
+    `08 Rosemary's Party.mp3`,
+    `16 Mary Clarke Theme.mp3`,
+    `04. Maurice Jarre – Sarah.mp3`,
+  ],
+  col4: [
+    `02 The Coven_Moment Musical.mp3`,
+    `09 Through the Closet.mp3`,
+    `12. Break It to My Heart.mp3`,
+  ],
+  col5: [`17 Chase the Hunchback.mp3`, `02.mp3`, `21 Track 21.mp3`],
+  col6: [
+    `10 What Have You Done to It's Eyes.mp3`,
+    `18 Kitty.mp3`,
+    `05 Expectancy.mp3`,
+  ],
+  col7: [`03 Dream.mp3`, `11 Happy Days.mp3`, `04 Christmas.mp3`],
+  col8: [
+    `19 Blood on My Hands_End Cast.mp3`,
+    `13 Love Theme - Lowry Dates Ann.mp3`,
+    `12 Main Title - Jack the Ripper - Th.mp3`,
+  ],
+  col9: [`shift.mp3`, `06 Main Title.mp3`],
+};
