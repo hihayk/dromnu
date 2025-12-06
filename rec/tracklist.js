2522476885369e9abe5a78fe5199cbc7b31e63c7
Hayk An <hihayk@gmail.com>
2025-12-06 15:18:38 +0100

    new audios

--- rec/tracklist.js
+++ rec/tracklist.js
@@ -1,29 +1,30 @@
 const tracklist = {
   col1: [
-
+    `02.mp3`
   ],
   col2: [
-    `FYW.mp3`
+    `arm noise2.mp3`
   ],
   col3: [
-
+    `fragments.mp3`
   ],
   col4: [
-    
+    `14914.mp3`
   ],
   col5: [
-    `02.mp3`,
+    `bass r2.mp3`,
+    `delay drums.mp3`,
   ],
   col6: [
-    
+    `ftg3.mp3`
   ],
   col7: [
-
+    `CHTR.mp3`
   ],
   col8: [
-    
+    `careful it's wt.mp3`
   ],
   col9: [
-    
+    `FYW.mp3`
   ],
 };
