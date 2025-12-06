d896c88eec01b53a55ee50a6352d82d4293f7327
Hayk <hayk@flodesk.com>
2025-12-06 17:54:44 +0100

    Update tracklist.js

--- rec/tracklist.js
+++ rec/tracklist.js
@@ -7,7 +7,7 @@ const tracklist = {
   ],
   col3: [
     `bass r2.mp3`,
-    `UFX95_00.mp3`,
+    `UFX28_00.mp3`,
   ],
   col4: [
     `14914.mp3`,
@@ -16,23 +16,24 @@ const tracklist = {
   // -----------------------
   col5: [
     `fragments.mp3`,
-    `UFX98_00.mp3`,
+    `02.mp3`,
   ],
   // -----------------------
   col6: [
-    `delay drums.mp3`,
-    `UFX30_00.mp3`,
-    `UFX28_00.mp3`,
+    `UFX98_00.mp3`,
   ],
   col7: [
     `CHTR.mp3`,
-    `ftg3.mp3`,
+    `delay drums.mp3`,
+    `UFX95_00.mp3`,
   ],
   col8: [
-    `UFX84_00.mp3`,
+    
+    // `UFX84_00.mp3`,
+    `UFX30_00.mp3`,
   ],
   col9: [
     `careful it's wt.mp3`,
-    `02.mp3`,
+    `ftg3.mp3`,
   ],
 };
