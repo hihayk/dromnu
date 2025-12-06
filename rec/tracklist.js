9ae28a7af2d3519d8e18b8b1adb3b5673ba23062
Hayk <hayk@flodesk.com>
2025-12-06 16:55:53 +0100

    Update tracklist.js

--- rec/tracklist.js
+++ rec/tracklist.js
@@ -6,20 +6,22 @@ const tracklist = {
     `arm noise2.mp3`
   ],
   col3: [
-    `fragments.mp3`
+    `bass r2.mp3`,
   ],
   col4: [
-    `14914.mp3`
+    `14914.mp3`,
   ],
+  // -----------------------
   col5: [
-    `bass r2.mp3`,
-    `delay drums.mp3`,
+    `fragments.mp3`
   ],
+  // -----------------------
   col6: [
     `ftg3.mp3`
   ],
   col7: [
-    `CHTR.mp3`
+    `CHTR.mp3`,
+    `delay drums.mp3`,
   ],
   col8: [
     `careful it's wt.mp3`
