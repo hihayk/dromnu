477c048c993a8db4194cd925132ae8c11e6775b9
Hayk An <hihayk@gmail.com>
2025-12-07 01:37:21 +0100

    css masonry

--- rec/tracklist.js
+++ rec/tracklist.js
@@ -1,39 +1,18 @@
-const tracklist = {
-  col1: [
-    `FYW.mp3`,
-  ],
-  col2: [
-    `arm noise2.mp3`,
-  ],
-  col3: [
-    `bass r2.mp3`,
-    `UFX28_00.mp3`,
-  ],
-  col4: [
-    `14914.mp3`,
-    `UFX97_00.mp3`,
-  ],
-  // -----------------------
-  col5: [
-    `fragments.mp3`,
-    `02.mp3`,
-  ],
-  // -----------------------
-  col6: [
-    `UFX98_00.mp3`,
-  ],
-  col7: [
-    `CHTR.mp3`,
-    `delay drums.mp3`,
-    `UFX95_00.mp3`,
-  ],
-  col8: [
-    
-    // `UFX84_00.mp3`,
-    `UFX30_00.mp3`,
-  ],
-  col9: [
-    `careful it's wt.mp3`,
-    `ftg3.mp3`,
-  ],
-};
+const tracklist = [
+  `FYW.mp3`,
+  `UFX28_00.mp3`,
+  `arm noise2.mp3`,
+  `bass r2.mp3`,
+  `UFX97_00.mp3`,
+  `14914.mp3`,
+  `UFX98_00.mp3`,
+  `fragments.mp3`,
+  `02.mp3`,
+  `CHTR.mp3`,
+  `careful it's wt.mp3`,
+  `delay drums.mp3`,
+  `UFX95_00.mp3`,
+  // `UFX84_00.mp3`,
+  `ftg3.mp3`,
+  `UFX30_00.mp3`,
+]
